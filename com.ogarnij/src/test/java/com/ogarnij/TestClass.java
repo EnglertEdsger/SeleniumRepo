dac8809e34cbe45c360ecef3c37fef5d7d0da5a8
JakubKaczynski <JakubKaczynski@DESKTOP-F58T3K5>
2018-08-13 13:46:43 +0200

    initial commit

---
+++ com.ogarnij/src/test/java/com/ogarnij/TestClass.java
@@ -0,0 +1,29 @@
+package com.ogarnij;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+public class TestClass {
+	final static Logger logger = Logger.getLogger(TestClass.class);
+
+	@BeforeTest
+	public void setUpChromeDriver() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\JakubKaczynski\\Downloads\\selenium-java-3.13.0\\chromedriver.exe");
+
+
+	}
+
+	@Test
+	public void callWebDriver() {
+		ChromeDriver driver = new ChromeDriver();
+
+		logger.error("chujniaMOrdo");
+		System.out.println("dupa");
+		driver.get("https://www.onet.pl");
+		
+	}
+
+}
